df1e1951333919d9b8395303c06351fc58768b99
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-08 04:07:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,4 @@
 board=[" "," "," "," "," "," "," "," "," "]
-input=gets.strip
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -7,11 +6,6 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(input)
-  input.to_i
-  index=input-1
 end
 
 def valid_move?(board, index)
